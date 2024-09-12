9ecf2f3b08a53476cb5acd0ce919843cba102248
LejoSan <alejandrosandovalp@outlook.com>
2024-09-12 19:20:21 +0200

    incio_2

--- Assets/Ejercicios/Ejecicio_1.cs
+++ Assets/Ejercicios/Ejecicio_1.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class Ejecicio_1 : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    string Nombre = "Alejandro Sandoval Paz";
     void Start()
     {
+
         Debug.Log("Hello world!");
+
+        Debug.Log("Este es el primer video juego " + Nombre + " al iniciar el juego.");
+
+        Debug.Log("Estoy aprendido C#");
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        //Debug.Log("Ha pasado un frame);
     }
 }
--- Assets/Ejercicios/Ejercicio_2.cs
+++ Assets/Ejercicios/Ejercicio_2.cs
@@ -4,6 +4,29 @@ using UnityEngine;
 
 public class Ejercicio_2 : MonoBehaviour
 {
+    //----------------------------------------------
+    int edad = 25;
+    int codigoPostal = 54796;
+    double altura = 1.87;
+    string genero = "Masculino";
+    string nombre = "Alejandro Sandoval";
+    int numeroDeHijos = 0;
+    double iva = 16;
+    char tallaCamisa;
+    float peso;
+    float precio;
+    string alumnoRepetidor;
+    string mensaje;
+    char letra;
+    int mayorEdad;
+    int minutos;
+    int dias;
+    string matriculaCoche;
+    int contador;
+    char TallaCamisetas;
+    //----------------------------------------------
+
+
     // Start is called before the first frame update
     void Start()
     {
