d78b83adab265e87c9470250ee8812d92b4a5bb3
LejoSan <alejandrosandovalp@outlook.com>
2024-09-17 11:25:18 +0200

    Bloque 5 terminado seudo codigo
    Todo esta comentado con procedimiento no esta complementado con código

--- Assets/Ejercicios/Ejercicio_5_1.cs
+++ Assets/Ejercicios/Ejercicio_5_1.cs
@@ -4,10 +4,62 @@ using UnityEngine;
 
 public class Ejercicio_5_1 : MonoBehaviour
 {
+    //bool X = true;
+    //bool Y = false;
+    //bool Z = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //A.  ((X && Y) || (X && Z))
+
+        //    (False) || (True)
+
+        //    R/. True
+
+        //-----------------------------------------------
+
+
+        //B.  (X || !Y) && (!X || Z)
+
+        //    (True) && (True)
+
+        //    R/. True
+
+        //-----------------------------------------------
+
+
+        //C.  (X) || Y && Z
+
+        //    (True) || (False)
+
+        //    R/. True
+
+        //-----------------------------------------------
+
+        //D.  !(X || Y) && Z
+
+        //    (False) && (True)
+
+        //    R/. False
+
+        //-----------------------------------------------
+
+        //E.  X || Y || X  && !Z && !Y 
+
+        //    (True) || (False)
+
+        //    R/. True
+
+        //-----------------------------------------------
+
+        //F.  !X || !Y || Z && X && !Y 
+
+        //    (True) || (True)
+
+        //    R/. True
+
+
     }
 
     // Update is called once per frame
--- Assets/Ejercicios/Ejercicio_5_2.cs
+++ Assets/Ejercicios/Ejercicio_5_2.cs
@@ -4,10 +4,60 @@ using UnityEngine;
 
 public class Ejercicio_5_2 : MonoBehaviour
 {
+
+    //bool W = false;
+    //bool X = true;
+    //bool Y = true;
+    //bool Z = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //A.  W || Y && X && W || Z 
+
+        //    (True) && (True) && (False)
+
+        //    R/. False
+
+        //-----------------------------------------------
+
+        //B.  X && !Y && !X || !W && Y 
+
+        //    (False) || (True)
+
+        //    R/. True
+
+        //-----------------------------------------------
+
+        //C.  !(W || !Y) && X || Z 
+
+        //    (True) || (False)
+
+        //    R/. True
+
+        //-----------------------------------------------
+
+        //D.   X && Y && W || Z || X 
+
+        //    (False) || (True)
+
+        //    R/. True
+
+        //-----------------------------------------------
+
+        //E.   Y || !(Y || Z && W )
+
+        //    (True) || (False)
+
+        //    R/. True
+
+        //-----------------------------------------------
+
+        //F.   !X && Y && (!Z || !X )
+
+        //    (False) && (True) && (True)
+
+        //    R/. False
     }
 
     // Update is called once per frame
